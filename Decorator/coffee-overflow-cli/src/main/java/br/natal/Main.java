cc2f643541ed5e958f0697f4341406e6ad90a6be
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-10 19:45:07 -0300

    feat(Decorator, CoffeeOverflowCLI): toppings getter and setter on AbstractBeverage interface

--- Decorator/coffee-overflow-cli/src/main/java/br/natal/Main.java
+++ Decorator/coffee-overflow-cli/src/main/java/br/natal/Main.java
@@ -1,7 +1,17 @@
 package br.natal;
 
+import br.natal.model.beverage.AbstractBeverage;
+import br.natal.model.beverage.concreteBeverages.Espresso;
+import br.natal.model.topping.Sugar;
+
 public class Main {
     public static void main(String[] args) {
+
+        AbstractBeverage espresso = new Espresso();
+//        espresso.setToppings(new Sugar());
+
         System.out.println("Hello world!");
+
+
     }
 }
--- Decorator/coffee-overflow-cli/src/main/java/br/natal/model/beverage/AbstractBeverage.java
+++ Decorator/coffee-overflow-cli/src/main/java/br/natal/model/beverage/AbstractBeverage.java
@@ -1,6 +1,11 @@
 package br.natal.model.beverage;
 
+import br.natal.model.topping.AbstractTopping;
+
 public interface AbstractBeverage {
     String getDescription();
     double getCost();
+
+    void setToppings(AbstractTopping toppings);
+    AbstractTopping getToppings();
 }
