480f6ab2b8b0d2a4edc33159d4fb79e8e25d2d5e
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-19 00:12:43 -0300

    feat(Decorator, CoffeeOverflowCLI): basic usage of Decorator pattern on Main

--- Decorator/coffee-overflow-cli/src/main/java/br/natal/Main.java
+++ Decorator/coffee-overflow-cli/src/main/java/br/natal/Main.java
@@ -2,15 +2,21 @@ package br.natal;
 
 import br.natal.model.beverage.AbstractBeverage;
 import br.natal.model.beverage.concreteBeverages.Espresso;
-import br.natal.model.topping.Sugar;
+import br.natal.model.topping.concreteToppings.Milk;
+import br.natal.model.topping.concreteToppings.Sugar;
+import br.natal.model.topping.concreteToppings.WhippedCream;
 
 public class Main {
     public static void main(String[] args) {
 
+        /**
+         *  Basic usage example
+         */
         AbstractBeverage espresso = new Espresso();
-//        espresso.setToppings(new Sugar());
+        espresso.setToppings(new Sugar(new WhippedCream(new Milk())));
 
-        System.out.println("Hello world!");
+        System.out.println("Espresso with:");
+        System.out.println(espresso.getToppings().name());
 
 
     }
