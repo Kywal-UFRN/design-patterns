8049d60e388a265826b83ecd0c5ed2239c640463
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-19 00:10:27 -0300

    feat(Decorator, CoffeeOverflowCLI): StringSeparator on Milk name method

--- Decorator/coffee-overflow-cli/src/main/java/br/natal/model/topping/concreteToppings/Milk.java
+++ Decorator/coffee-overflow-cli/src/main/java/br/natal/model/topping/concreteToppings/Milk.java
@@ -3,15 +3,20 @@ package br.natal.model.topping.concreteToppings;
 import br.natal.model.topping.AbstractTopping;
 import br.natal.model.topping.ToppingType;
 import br.natal.model.topping.ToppingWrapper;
+import br.natal.utils.StringSeparator;
 
 public class Milk extends ToppingWrapper {
+
+    public Milk() {
+        super();
+    }
 
     public Milk(AbstractTopping topping) {
         super(topping);
     }
 
     public String name() {
-        return topping.name() + ToppingType.Milk.name;
+        return topping.name() + StringSeparator.BASIC.begin + ToppingType.Milk.name + StringSeparator.BASIC.end;
     }
 
     public double cost() {
