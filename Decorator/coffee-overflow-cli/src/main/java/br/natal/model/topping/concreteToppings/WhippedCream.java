18845da8474308e4598638a64fcbeb1dbc3d5f94
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-19 00:10:46 -0300

    feat(Decorator, CoffeeOverflowCLI): StringSeparator on WhippedCream name method

--- Decorator/coffee-overflow-cli/src/main/java/br/natal/model/topping/concreteToppings/WhippedCream.java
+++ Decorator/coffee-overflow-cli/src/main/java/br/natal/model/topping/concreteToppings/WhippedCream.java
@@ -3,15 +3,20 @@ package br.natal.model.topping.concreteToppings;
 import br.natal.model.topping.AbstractTopping;
 import br.natal.model.topping.ToppingType;
 import br.natal.model.topping.ToppingWrapper;
+import br.natal.utils.StringSeparator;
 
 public class WhippedCream extends ToppingWrapper {
+
+    public WhippedCream() {
+        super();
+    }
 
     public WhippedCream(AbstractTopping topping) {
         super(topping);
     }
 
     public String name() {
-        return topping.name() + ToppingType.WhippedCream.name;
+        return topping.name() + StringSeparator.BASIC.begin + ToppingType.WhippedCream.name + StringSeparator.BASIC.end;
     }
 
     public double cost() {
