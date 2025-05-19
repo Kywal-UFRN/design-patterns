c8ab2c286023eca307e6bfd9092dbeffdc5a9169
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-19 00:10:37 -0300

    feat(Decorator, CoffeeOverflowCLI): StringSeparator on Sugar name method

--- Decorator/coffee-overflow-cli/src/main/java/br/natal/model/topping/concreteToppings/Sugar.java
+++ Decorator/coffee-overflow-cli/src/main/java/br/natal/model/topping/concreteToppings/Sugar.java
@@ -3,15 +3,20 @@ package br.natal.model.topping.concreteToppings;
 import br.natal.model.topping.AbstractTopping;
 import br.natal.model.topping.ToppingType;
 import br.natal.model.topping.ToppingWrapper;
+import br.natal.utils.StringSeparator;
 
 public class Sugar extends ToppingWrapper {
+
+    public Sugar() {
+        super();
+    }
 
     public Sugar(AbstractTopping topping) {
         super(topping);
     }
 
     public String name() {
-        return topping.name() + ToppingType.Sugar.name;
+        return topping.name() + StringSeparator.BASIC.begin + ToppingType.Sugar.name + StringSeparator.BASIC.end;
     }
 
     public double cost() {
