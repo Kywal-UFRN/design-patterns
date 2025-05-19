6d88544e22483451adb426d2571b885bc510f7fd
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-18 23:29:28 -0300

    feat(Decorator, CoffeeOverflowCLI): enum ToppingType
    content of concrete toppings Milk, Sugar and Whipped Cream

---
+++ Decorator/coffee-overflow-cli/src/main/java/br/natal/model/topping/concreteToppings/ToppingType.java
@@ -0,0 +1,15 @@
+package br.natal.model.topping.concreteToppings;
+
+public enum ToppingType {
+    Milk("Milk", 0.5),
+    Sugar("Sugar", 0.10),
+    WhippedCream("Whipped Cream", 0.5);
+
+    public final String name;
+    public final double cost;
+
+    ToppingType(String name, double cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+}
