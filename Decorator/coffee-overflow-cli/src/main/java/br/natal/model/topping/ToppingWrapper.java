f011d619edcfcf84f7d8c8fd67c6964794523e20
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-19 00:09:01 -0300

    feat(Decorator, CoffeeOverflowCLI): empty constructor with NoTopping class instantiation

--- Decorator/coffee-overflow-cli/src/main/java/br/natal/model/topping/ToppingWrapper.java
+++ Decorator/coffee-overflow-cli/src/main/java/br/natal/model/topping/ToppingWrapper.java
@@ -1,7 +1,13 @@
 package br.natal.model.topping;
 
+import br.natal.model.topping.concreteToppings.NoTopping;
+
 public abstract class ToppingWrapper implements AbstractTopping {
     protected AbstractTopping topping;
+
+    public ToppingWrapper() {
+        this.topping = new NoTopping();
+    }
 
     public ToppingWrapper(AbstractTopping topping) {
         this.topping = topping;
